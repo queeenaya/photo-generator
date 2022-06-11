56260e74da525dcf3a397aa76092ea301ab300bb
Ayazhan <78973046+queeenaya@users.noreply.github.com>
2022-06-11 23:39:53 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+const auth = "563492ad6f9170000100000178011ccbca7d4bb787bedf55e6b067dc";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const formBtn = document.querySelector(".search-form");
+const moreBtn = document.querySelector(".more");
+let searchValue;
+let page = 1;
+let fetchLink;
+let currentSearch;
+
+//Event Listeners
+searchInput.addEventListener("input", updateInput);
+formBtn.addEventListener("submit", (e) => {
+    e.preventDefault();
+    currentSearch = searchValue;
+    searchPhotos(searchValue);
+});
+
+moreBtn.addEventListener("click", loadMore);
+
+function updateInput(e) {
+    searchValue = e.target.value;
+}
+
+async function fetchApi(url) {
+    const dataFetch = await fetch(url, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization: auth
+        }
+    });
+    const data = await dataFetch.json();
+    return data;
+}
+
+function generatePictures(data) {
+    data.photos.forEach(photo => {
+        const galleryImg = document.createElement("div");
+        galleryImg.classList.add("gallery-img");
+        galleryImg.innerHTML = 
+        `
+            <div class="gallery-info">
+                <a href=${photo.photographer_url} target="_blank">${photo.photographer}</a>
+                <a href=${photo.src.original} target="_blank">View full image</a>
+            </div>
+            <img src="${photo.src.large}"></img>
+        `;
+        gallery.appendChild(galleryImg);
+    });
+}
+
+async function curatedPhotos() {
+    fetchLink = "https://api.pexels.com/v1/curated?per_page=15&page=1";
+    const data = await fetchApi(fetchLink);
+    generatePictures(data);
+}
+
+async function searchPhotos(query) {
+    clear();
+    fetchLink = `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`;
+    const data = await fetchApi(fetchLink);
+    generatePictures(data);
+}
+
+async function loadMore() {
+    page++;
+    if (currentSearch) {
+        fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}+query&per_page=15&page=${page}`;
+    } else {
+        fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`
+    }
+    const data = await fetchApi(fetchLink);
+    generatePictures(data);
+}
+
+function clear() {
+    gallery.innerHTML = "";
+    searchInput.value = "";
+}
+
+curatedPhotos();
